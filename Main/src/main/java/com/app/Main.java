5790a4519916e0da3c261905573ed63080ea388c
ShubhamSirsat <Shubhamsirsat2905@gmail.com>
2022-07-04 11:47:36 +0530

    Maven project is Added

---
+++ Main/src/main/java/com/app/Main.java
@@ -0,0 +1,14 @@
+package com.app;
+
+public class Main {
+public static void main(String[] args)
+{
+	System.out.println(checkIfInputIsEvenNumber(122));
+	// Testing in the main method
+	
+}
+public static boolean checkIfInputIsEvenNumber(int number)
+{
+	return number%2==0;
+}
+}
